9c20c0050642a19835769196666b424206567d27
kurt21 <kurtyjang@gmail.com>
2022-05-10 21:09:14 -0400

    9PM changes

--- script.js
+++ script.js
@@ -5,14 +5,33 @@ function start(){
     $("ul.nav a:first").click()
 }
 
-$(document).on("click", "ul.nav a", nav)    
 
-function nav(e){
-    e.preventDefault();
-    var url = $(this).attr("href");
-    $("#content").load(url);
-    $("ul.nac a").removeClass("active");
-    $(this).addClass("active");
+$("#menu_NEWIN").click(openNEWIN);
 
+function openNEWIN(){
+	$("#menu a").removeClass("active");
+	$(this).addClass("active");
+	$("#content_NEWIN").siblings().hide();
+	$("#content_NEWIN").show();
 }
 
+$("#menu_EXPLORE").click(openEXPLORE);
+
+function openEXPLORE(){
+	$("#menu a").removeClass("active");
+	$(this).addClass("active");
+	$("#content_EXPLORE").siblings().hide();
+	$("#content_EXPLORE").show();
+}
+
+$("#menu_LOOKBOOK").click(openLOOKBOOK);
+
+function openLOOKBOOK(){
+	$("#menu a").removeClass("active");
+	$(this).addClass("active");
+	$("#content_LOOKBOOK").siblings().hide();
+	$("#content_LOOKBOOK").show();
+}
+
+
+$("#menu_home").click();
